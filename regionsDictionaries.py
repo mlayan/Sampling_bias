05b9ef17c6985e9cf93af7eeecc62edc98f633aa
mlayan <59015133+mlayan@users.noreply.github.com>
2020-07-27 10:26:58 +0200

    Add files via upload

---
+++ regionsDictionaries.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+SET OF DICTIONARIES FOR REGIONS
+"""
+
+# Description  
+__author__ = 'Maylis Layan'
+__creation_date__ = '2020-04-30' 
+
+
+##################################################
+# Regions dictionaries
+##################################################
+
+# Patch - regions dictionary
+regionDictionary = {
+	1:'Western Mediterranean coast', 
+	2 : 'Eastern Mediterranean coast', 
+	3: 'North Atlantic coast', 
+	4: 'South Atlantic coast', 
+	5: 'Northern Atlas', 
+	6: 'Southern Atlas', 
+	7: 'Anti Atlas'
+}
+
+
+# Short regions dictionary
+regionDictionary2 = {
+	'WesternMc':'Western Mediterranean coast', 
+	'EasternMc' : 'Eastern Mediterranean coast', 
+	'NorthAc': 'North Atlantic coast', 
+	'SouthAc': 'South Atlantic coast', 
+	'NorthA': 'Northern Atlas', 
+	'SouthA': 'Southern Atlas', 
+	'AntiA': 'Anti Atlas'
+}
+
+
+##################################################
+# Regions list
+##################################################
+
+# All regions
+allRegions=[
+	'WesternMediterraneancoast', 
+	'EasternMediterraneancoast', 
+	'NorthAtlanticcoast', 
+	'SouthAtlanticcoast', 
+	'NorthernAtlas', 
+	'SouthernAtlas',
+	'AntiAtlas'
+]
