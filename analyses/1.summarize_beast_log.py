a525685912066c1f46b57901592ea97707989628
mlayan <59015133+mlayan@users.noreply.github.com>
2020-08-21 12:04:08 +0200

    Update 1.summarize_beast_log.py

--- analyses/1.summarize_beast_log.py
+++ analyses/1.summarize_beast_log.py
@@ -2,80 +2,74 @@
 # coding: utf-8
 
 """
-ANALYZE SIMULATION FILES 
-- Write Newick trees
-- Compute migration events and regions frequencies
-- Compute association index of the tree 
+ANALYZE BEAST LOG FILES
 """
 
 # Description  
 __author__ = 'Maylis Layan'
-__creation_date__ = '2020-05-06' 
-__last_update__ = '2020-05-'
+__creation_date__ = '2020-02-28' 
+__last_update__ = '2020-05-13'
 
 # Import libraries
 import os
 import sys
 import re
-import math
-from concurrent.futures import ProcessPoolExecutor
 import pandas as pd
-import numpy as np
+from concurrent.futures import ProcessPoolExecutor
 
 ## Import custom functions
 sys.path.append('../python/')
-from simulatedTrees import *
+from geneticAndSpatialEstimates import *
+
+###############################################
+## SUM UP  
+# Runtype
+runType = "dta"
 
 ## Directory
 cond = 'mig' + str(sys.argv[1])
 directory = "../" + cond
 os.chdir(directory)
 
-## Region dictionary
-regionDic = {
-1: "Region1",
-2: "Region2",
-3: "Region3",
-4: "Region4",
-5: "Region5",
-6: "Region6",
-7: "Region7"
+# Dictionary of the regions
+regionDict = {
+    1: "Region1",
+    2: "Region2",
+    3: "Region3",
+    4: "Region4",
+    5: "Region5",
+    6: "Region6",
+    7: "Region7"
 }
 
-##########################################
 # Load log files
 logFiles = []
 directories = []
-nFiles = 0
 
 for root, subdirs, files in os.walk('.'):
-	if 'files' in root:
+	if runType in root:
 		for f in files:
-			if 'transmission_chain' in f:
-				logFiles += [f] * len(protocols)
-				directories += [root] * len(protocols)
-				nFiles += 1
-
-protocols += protocols * (nFiles - 1)
-
-
-# Helper function to pass correctly arguments to the mapper
-def helperF(f,p,d):
-    return(migrationEvents(f, cond, p, regionDic = regionDic,
-    	extractNewickTree=True, directory=d))
+			if 'log.txt' in f:
+				logFiles.append(f)
+				directories.append(root)
 
 
 # Get summary tables for each Beast run
+def helperF(f,d):
+    return(logFileWrangler(f, d, cond, runType, regionDict,
+        rootLocation = True, ess = True))
+
 with ProcessPoolExecutor() as executor:
-    results = []
-    for result in executor.map(helperF, logFiles, protocols, directories):
-        results.append(result)
+    out = []
+    
+    for result in executor.map(helperF, logFiles, directories):
+        out.append(result)
 
 # Concatenate dataframes
-data = pd.concat(results, axis = 0)
+data = pd.concat(out, axis = 0)
 
 # Write the dataframe
-data.to_csv('../analyses/migration_events_sim1to10_' + cond + '.txt', 
+data.to_csv('../analyses/log_files_' + runType + '_' + cond + '_ESS.txt', 
             index = False, 
             header = True, 
             sep = '\t', 
